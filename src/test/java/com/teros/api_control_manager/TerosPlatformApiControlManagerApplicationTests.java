7939f5affca528444a7db598b76acfa4aeddd87c
navana <onlywin7788@gmail.com>
2021-11-29 15:22:14 +0900

    commit

---
+++ src/test/java/com/teros/api_control_manager/TerosPlatformApiControlManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teros.api_control_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerosPlatformApiControlManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
